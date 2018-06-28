a5bf826ef4d2f608fb11a95be690ec0b3ca95ccf
graypeck <Graydon.Peck@Ally.com>
2018-06-28 15:07:41 -0400

    here

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 
 const express = require('express');
 const app = express();
-const path = require('path');// If an incoming request uses
+const path = require('path');
+// If an incoming request uses
 // a protocol other than HTTPS,
 // redirect that request to the
 // same url but with HTTPS
